8e8199b663f9dcce7aa172d8d58a257c73fff6b4
nahid <nahidrabbi045@gmail.com>
2024-03-25 09:06:41 +0600

    remove mark

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,7 +1,7 @@
 import PropTypes from 'prop-types';
 import { FaBookmark } from 'react-icons/fa';
 const Blog = ({blog,handleAddToBookmark,handleMarkAsRead}) => {
-    const {title, cover,author,author_img,posted_date,reading_time,hashtags} = blog;
+    const {id,title, cover,author,author_img,posted_date,reading_time,hashtags} = blog;
     return (
         <div className='mb-20 space-y-4'>
             <img className='w-full mt-4 mb-8 rounded-md' src={cover} alt={`cover picture of the title ${title}`} />
@@ -31,7 +31,7 @@ const Blog = ({blog,handleAddToBookmark,handleMarkAsRead}) => {
                     </span>)
                 }
             </p>
-            <button onClick= {()=> handleMarkAsRead(reading_time)}className='text-purple-800 font-bold underline'>Mark As Read</button>
+            <button onClick= {()=> handleMarkAsRead(id,reading_time)}className='text-purple-800 font-bold underline'>Mark As Read</button>
         </div>
     );
 };
